59d2d2620b3e81da529acba714590275c7a1c9d9
nsriabhinav <64884634+nsriabhinav@users.noreply.github.com>
2020-09-18 09:24:32 +0530

    Add files via upload

--- sum.c
+++ sum.c
@@ -1,15 +1,27 @@
-#include<stdio.h>
+#include <stdio.h>
+int sumofno(int num);
+
+
 int main()
 {
-	int n, r, sum=0;
-	printf("enter a number:");
-	scanf("%d", &n);
-	while(n!=0)
-	{
-		r = n% 10;
-		sum+= r;
-		n= n/10;
-	}
-	printf("sum=%d",sum);
-	return 0;
+    int num, sum;
+    
+    printf("Enter any number to find sum of digits: ");
+    scanf("%d", &num);
+    
+    sum = sumofno(num);
+    
+    printf("Sum of digits of %d = %d", num, sum);
+    
+    return 0;
+}
+
+
+int sumofno(int num)
+{
+    // Base condition
+    if(num == 0)
+        return 0;
+        
+    return ((num % 10) + sumofno(num / 10));
 }
