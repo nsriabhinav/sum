cff54f539450fc0e9419ab05cc119db845a42fec
nsriabhinav <64884634+nsriabhinav@users.noreply.github.com>
2020-09-18 09:11:25 +0530

    Add files via upload

---
+++ sum.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+	int n, r, sum=0;
+	printf("enter a number:");
+	scanf("%d", &n);
+	while(n!=0)
+	{
+		r = n% 10;
+		sum+= r;
+		n= n/10;
+	}
+	printf("sum=%d",sum);
+	return 0;
+}
